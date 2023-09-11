52566a9d35d86b6e8e7e3ff4be87699ba7aa22b0
Denzel Huijbers <huijbers15@outlook.com>
2023-09-11 10:52:22 +0200

    Add and remove tags from tag list

--- wwwroot/js/MonsterTableFilters.js
+++ wwwroot/js/MonsterTableFilters.js
@@ -5,8 +5,36 @@
 $(document).ready(function () {
 
     $('#size-filter').on('change', function () {
-        sizeFilters.push(this.value);
-        console.log('hit');
-        console.log(sizeFilters);
+        //console.log(sizeFilters);
+        UpdateTags(this.value);
+        console.log(sizeFilters)
+
+       
     });
 });
+
+
+function UpdateTags(tag) {
+    if (sizeFilters.includes(tag)) {
+        console.log("tag already selected");
+        RemoveTag(tag);
+    }
+    else {
+        var spanTag = $(`<span class='filter-tag' id='${tag}'></span>`).text(tag);
+       
+        $(".filter-tags").append(spanTag);
+        sizeFilters.push(tag);
+        
+    }
+}
+
+function RemoveTag(tag) {
+    var index = sizeFilters.indexOf(tag);
+    if (index !== -1) {
+        sizeFilters.splice(index, 1);
+        var tagToRemove = document.getElementById(tag)
+        tagToRemove.remove();
+
+    }
+    
+}
