cb4890a966c903f5d02e8027b100d61c92b32501
Denzel Huijbers <huijbers15@outlook.com>
2023-09-11 09:51:55 +0200

    Added javascript file and select for filters

---
+++ wwwroot/js/MonsterTableFilters.js
@@ -0,0 +1,12 @@
+﻿var sizeFilters = [];
+
+
+
+$(document).ready(function () {
+
+    $('#size-filter').on('change', function () {
+        sizeFilters.push(this.value);
+        console.log('hit');
+        console.log(sizeFilters);
+    });
+});
